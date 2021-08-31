e4a1cfca29627e695d038ff5fcfe6905e84fedf0
vinidesenvolve <vinidesenvolve@gmail.com>
2021-08-30 23:13:33 -0300

    Upload de arquivo e listagem de dados da API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TransactionService } from './service/transaction.service';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TransactionService, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
